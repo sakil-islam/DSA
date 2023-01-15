ec190c7d0f76af7f137bf151e5c411807ebade63
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-01-15 12:48:30 +0600

    second approach 2 different sorted array converting a sorted array

--- Sorting/2arrayCon1SortArray.js
+++ Sorting/2arrayCon1SortArray.js
@@ -1,33 +1,33 @@
 let arrayA = [10, 20];
 let arrayB = [10, 20];
-// JavaScript program to merge two sorted arrays/
-// function mergeArrays(arr1, arr2, n1, n2, arr3) {
-//     var i = 0, j = 0, k = 0;
+// JavaScript program to merge two sorted arrays
+function mergeArrays(arr1, arr2, n1, n2, arr3) {
+    var i = 0, j = 0, k = 0;
 
-//     // traverse the arr1 and insert its element in arr3
-//     while (i < n1) {
-//         arr3[k++] = arr1[i++];
-//     }
+    // traverse the arr1 and insert its element in arr3
+    while (i < n1) {
+        arr3[k++] = arr1[i++];
+    }
 
-//     // now traverse arr2 and insert in arr3
-//     while (j < n2) {
-//         arr3[k++] = arr2[j++];
-//     }
+    // now traverse arr2 and insert in arr3
+    while (j < n2) {
+        arr3[k++] = arr2[j++];
+    }
 
-//     // sort the whole array arr3
-//     // arr3.sort();
-// }
-// var arr1 = [1, 3, 5, 7];
-// var n1 = arr1.length;
-// var arr2 = [2, 4, 6, 8];
-// var n2 = arr2.length;
-// var arr3 = new Array(n1 + n2);
-// mergeArrays(arr1, arr2, n1, n2, arr3);
+    // sort the whole array arr3
+    // arr3.sort();
+}
+var arr1 = [1, 3, 5, 7];
+var n1 = arr1.length;
+var arr2 = [2, 4, 6, 8];
+var n2 = arr2.length;
+var arr3 = new Array(n1 + n2);
+mergeArrays(arr1, arr2, n1, n2, arr3);
 
-// console.log("Array after merging");
-// for (var i = 0; i < n1 + n2; i++) {
-//     process.stdout.write(arr3[i] + " ");
-// }
+console.log("Array after merging");
+for (var i = 0; i < n1 + n2; i++) {
+    process.stdout.write(arr3[i] + " ");
+}
 
 let arrayCon1SortArray = function (arr1, arr2) {
     let i = 0;
