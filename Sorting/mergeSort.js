b618cf8f6196f16ada908de236a423c706672ce7
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-01-15 14:37:26 +0600

    merge sort implement in javascript.

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,53 @@
+/* 
+Algorithm:
+
+step 1: start
+
+step 2: declare array and left, right, mid variable
+
+step 3: perform merge function.
+    if left > right
+        return
+    mid= (left+right)/2
+    mergesort(array, left, mid)
+    mergesort(array, mid+1, right)
+    merge(array, left, mid, right)
+
+step 4: Stop
+
+*/
+
+function merge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let results = [];
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+console.log(mergeSort([30, 70, 10, 5, 1, 22]));
+
