854eb7a514b958bb229e0ffe27cd1177955924dc
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-01-15 14:41:15 +0600

    second approach merge sort implement in javascript

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -51,3 +51,63 @@ function mergeSort(arr) {
 }
 console.log(mergeSort([30, 70, 10, 5, 1, 22]));
 
+// DSA course Approach...
+
+let array = [20, 10, 30, 50, 40, 30]; 
+let newArray = []; 
+
+
+function mergeSortFunc(array, l, r) {
+    if(l < r) { 
+        let mid = Math.floor((l+r) / 2); 
+        mergeSortFunc(array, l, mid); // left
+        mergeSortFunc(array, mid+1, r); // right
+
+        // here sub-arrayay is already sorted
+
+        mergeSubarray(array, l, mid, r); 
+    } 
+} 
+
+function mergeSubarray(array, l, mid, r) { 
+    let i = l; 
+    let j = mid+1; 
+    let k = i; 
+
+    while(i <= mid && j <= r) {
+        if(array[i] <= array[j]) { 
+            newArray[k] = array[i]; 
+            i++; 
+        } 
+        else {
+            newArray[k] = array[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+
+    // copy rest values
+    if(i > mid) {
+        while(j <= r) {
+            newArray[k] = array[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+    else { 
+        while(i <= mid) { 
+            newArray[k] = array[i]; 
+            i++; 
+            k++; 
+        } 
+    } 
+
+    // copy to original array
+    for(let z = l; z <= r; z++) {
+        array[z] = newArray[z]; 
+    }
+} 
+
+
+mergeSortFunc(array, 0, array.length-1); 
+console.log(array);
