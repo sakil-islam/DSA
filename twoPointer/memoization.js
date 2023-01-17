551a12bc9dde74f474e57f0344cfacaffc969443
sakil-islam <sakilislam1080@gmail.com>
2023-01-17 21:29:22 +0600

    factorial memoization code and prime in js

---
+++ twoPointer/memoization.js
@@ -0,0 +1,19 @@
+let factorialMemo = [];
+
+function factorialValueRemember(num) {
+    if (factorialMemo[num]) {
+        return factorialMemo[num];
+    }
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result = result * i;
+    }
+    factorialMemo[num] = result;
+    return result;
+}
+
+let arr = [3, 5, 5, 5, 5, 7, 8, 9, 3, 3, 3, 3];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(factorialValueRemember(arr[i]));
+}
