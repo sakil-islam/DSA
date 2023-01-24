da6f9c10c031b2ca41943bb1fa35c1e9e44cdb71
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-01-24 16:58:34 +0600

    BFS predecessor and successor implemented

--- BinaryTree/binaryTree.js
+++ BinaryTree/binaryTree.js
@@ -34,12 +34,68 @@ class BinaryTree {
             }
         }
     }
+
+    search(value){
+        let currentNode = this.root;
+        while(currentNode){
+            if(currentNode.value === value){
+                return 'Data found';
+            }
+            else if(currentNode.value > value){
+                currentNode = currentNode.left;
+            }
+            else{
+                currentNode = currentNode.right;
+            }
+        }
+        return 'Not found';
+    }
+    predecessor(){
+        let currentNode = this.root.left;
+        while(true){
+            if(!currentNode.right){
+                return currentNode.value;
+            }
+            currentNode = currentNode.right;
+        }
+    }
+    successor(){
+        let currentNode = this.root.right;
+        while(true){
+            if(!currentNode.left){
+                return currentNode.value;
+            }
+            currentNode = currentNode.left;
+        }
+    }
+    BFS(){
+        let queue = [];
+        queue.push(this.root);
+
+        while(queue.length){
+            let current = queue[0];
+            console.log(current.value);
+
+            if(current.left){
+                queue.push(current.left);
+            }
+            if(current.right){
+                queue.push(current.right);
+            }
+
+            queue.shift();
+        }
+    }
 }
 
-let tree = new BinaryTree(20);
+let tree = new BinaryTree(10);
 
-tree.insert(30);
+tree.insert(20);
 tree.insert(8);
+tree.insert(9);
+tree.insert(90);
 tree.insert(15);
-tree.insert(25);
-console.log(tree);
+console.log(tree.search(15));
+console.log(tree.predecessor());
+console.log(tree.successor());
+console.log(tree.BFS());
