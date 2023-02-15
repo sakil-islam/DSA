207ab0449ef0032cb008b608ed0730ae8d7c50b4
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-02-15 12:21:22 +0600

    Best approach to solve gcd

--- Array/GCD/1979.js
+++ Array/GCD/1979.js
@@ -17,3 +17,11 @@ var findGCD = function (nums) {
 };
 
 console.log(findGCD(array));
+
+// const gcd = (a, b) => b == 0 ? a : gcd(b, a % b);
+
+// const findGCD = (a) => {
+//     let max = Math.max.apply(Math, a);
+//     let min = Math.min.apply(Math, a);
+//     return gcd(min, max);
+// };
