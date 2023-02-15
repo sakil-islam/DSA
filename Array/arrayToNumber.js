59f3c79c9c3bf528fdcab41ef29b9305fdb90542
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-02-15 12:38:11 +0600

    array to number conversion process.

---
+++ Array/arrayToNumber.js
@@ -0,0 +1,18 @@
+const digits = [1, 2, 3, 4, 5];
+const number = parseInt(digits.join(''), 10);
+console.log(number); // Output: 12345
+
+/* 
+    const digits = [1, 2, 3, 4, 5];
+    const number = Number(digits.join(''));
+    console.log(number); // Output: 12345 
+    
+*/
+
+/*
+const digits = [1, 2, 3, 4, 5];
+const number = digits.reduce((acc, curr) => acc * 10 + curr, 0);
+console.log(number); // Output: 12345
+
+*/
+
