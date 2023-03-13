4f88805e354038c45d9e42f3308169ab00001bf4
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-03-13 10:22:06 +0600

    leet code 1512: Number of Good Pairs

---
+++ Array/1512.js
@@ -0,0 +1,15 @@
+// 1512. Number of Good Pairs
+
+var numIdenticalPairs = function (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i < j && nums[i] == nums[j]) {
+                count++
+            }
+        }
+    }
+    return count;
+};
+
+console.log(numIdenticalPairs([1, 1, 1, 1]))
