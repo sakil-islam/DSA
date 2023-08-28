2bc215394e2b7be0c247c0abf7435303c0ab49c0
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-08-28 10:22:57 +0600

    735: Asteroid Collision

---
+++ Stack/735.js
@@ -0,0 +1,14 @@
+var asteroidCollision = function(asteroids) {
+    let stack = []
+    for ( let a of asteroids ) {
+        if ( a < 0 ) {
+            while ( stack.at(-1) > 0 && stack.at(-1) < -a ) stack.pop()
+            if ( stack.at(-1) + a == 0 && stack.pop() ) continue
+            if ( stack.at(-1) > 0 && stack.at(-1) > -a ) continue
+        }
+        stack.push(a)
+    }
+    return stack
+}
+
+console.log(asteroidCollision([5,10,-5]));
